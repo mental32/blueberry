619756574356fee67bab71e8107446726665dd5e
mental <m3nta1@yahoo.com>
2018-10-14 11:47:38 +0100

    add requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import re
 from distutils.core import setup
 
+requirements = [
+	'websockets',
+	'flask'
+]
+
 def get_version():
     with open('blueberry/__main__.py') as inf:
       match = re.search(r"((\d\.){2,5}\d)", inf.read(), re.MULTILINE)
@@ -13,4 +18,5 @@ setup(
     name='blueberry',
     version=get_version(),
     description='Decentralised virtual network application.',
+    install_requires=requirements,
 )
