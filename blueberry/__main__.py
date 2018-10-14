515864f1a459047e10d1720558883f645df0e3c5
mental <m3nta1@yahoo.com>
2018-10-14 11:44:05 +0100

    Better arg parsing

--- blueberry/__main__.py
+++ blueberry/__main__.py
@@ -13,22 +13,11 @@ from blueberry import Blueberry
 
 
 def main(args):
-    port = args.port
-
-    if args.parent is None and not args.singlehost:
-        sys.exit('blueberry: missing operand "parent"')
-
-    if not args.singlehost:
-        parent = args.parent
-    else:
-        parent = None
-
-    app = Blueberry(parent, port)
+    app = Blueberry(args.parent, args.port)
     app.run()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('parent', metavar='parent', nargs='?')
     parser.add_argument('-p', dest='port', default='3722', type=int)
-    parser.add_argument('--standalone', dest='singlehost', action='store_true')
+    parser.add_argument('parent', metavar='parent', nargs='?')
     main(parser.parse_args())
