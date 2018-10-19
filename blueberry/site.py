753ead1d0dfc3555d3f5486317841c07d974d087
mental <m3nta1@yahoo.com>
2018-10-19 14:18:02 +0100

    change indentation to spaces

--- blueberry/site.py
+++ blueberry/site.py
@@ -1,13 +1,19 @@
+import os
+
 import flask
+
+import blueberry
 
 public = flask.Blueprint(__name__, 'index')
 private = flask.Blueprint(__name__ + '_api', 'api', url_prefix='/api/v1')
 
+
 @public.route('/')
 @public.route('/index')
 def index():
-	return flask.render_template('index.html')
+    return flask.render_template('index.html', app=blueberry._blueberries[os.getpid()])
+
 
 @private.route('/ping')
 def pong():
-	return 'OK'
+    return 'OK'
