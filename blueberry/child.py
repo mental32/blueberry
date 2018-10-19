89d6cb0f01c9e6957159fd50e98aac74a4b83cf6
mental <m3nta1@yahoo.com>
2018-10-19 14:07:20 +0100

    change indentation to spaces

--- blueberry/child.py
+++ blueberry/child.py
@@ -1,19 +1,18 @@
-
 class Child:
-	def __init__(self, ws, data):
-		self._ws = ws
-		self._data = data
+    def __init__(self, ws, data):
+        self._ws = ws
+        self._data = data
 
-	@property
-	def ws(self):
-		return self._ws
+    @property
+    def ws(self):
+        return self._ws
 
-	@property
-	def data(self):
-		return self._data	
+    @property
+    def data(self):
+        return self._data
 
-	def send(self, *args, **kwargs):
-		return self._ws.send(*args, **kwargs)
+    def send(self, *args, **kwargs):
+        return self._ws.send(*args, **kwargs)
 
-	def recv(self, *args, **kwargs):
-		return self._ws.recv(*args, **kwargs)
+    def recv(self, *args, **kwargs):
+        return self._ws.recv(*args, **kwargs)
